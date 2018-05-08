abdd8fdda2d41e1bbecbe02c6111c9b215a92612
Ryan Owens <rowens794@gmail.com>
2018-05-07 22:14:05 -0400

    working app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var parser = require('ua-parser-js');
+const requestIp = require('request-ip');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var software = parser(req.headers['user-agent'])['os'].name + "; " + parser(req.headers['user-agent'])['os'].version;
+  var language = req.headers['accept-language'].split(",")[0];
+  const clientIp = requestIp.getClientIp(req); 
+  object = {
+    "ipaddress": clientIp,
+    "language": language,
+    "software": software
+    }
+  res.send(object);
 });
 
 module.exports = router;
